cf9ac92b28252f7bf052feb748264073ba775caa
Vibhor Chinda <vibhorchinda@gmail.com>
2020-03-31 01:29:50 +0530

    added bundle to pass arguments

--- app/src/main/java/com/example/navigate/fragments/ChooserRecipientFragment.kt
+++ app/src/main/java/com/example/navigate/fragments/ChooserRecipientFragment.kt
@@ -2,15 +2,19 @@ package com.example.navigate.fragments
 
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
 import com.example.navigate.R
+import kotlinx.android.synthetic.main.fragment_chooser_recipient.*
 
 class ChooserRecipientFragment : Fragment(), View.OnClickListener {
 
@@ -30,8 +34,15 @@ class ChooserRecipientFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.cancel_btn -> activity!!.onBackPressed()
-            R.id.next_btn -> navController!!.navigate(R.id.action_chooserRecipientFragment_to_specifyAmountFragment)
+            R.id.next_btn -> {
+                if (!TextUtils.isEmpty(input_recipient.text.toString())) {
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController!!.navigate(R.id.action_chooserRecipientFragment_to_specifyAmountFragment, bundle)
+                }
+                else{
+                    Toast.makeText(context, "Please enter recipient name", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
-
 }
