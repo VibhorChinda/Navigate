e922377637234f585740186fa084ec34762fcaf7
Vibhor Chinda <vibhorchinda@gmail.com>
2020-03-30 21:59:31 +0530

    added navigation

--- app/src/main/java/com/example/navigate/fragments/ChooserRecipientFragment.kt
+++ app/src/main/java/com/example/navigate/fragments/ChooserRecipientFragment.kt
@@ -6,18 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.navigate.R
 
-class ChooserRecipientFragment : Fragment() {
+class ChooserRecipientFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    private lateinit var navController: NavController
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_chooser_recipient, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.cancel_btn -> activity!!.onBackPressed()
+            R.id.next_btn -> navController!!.navigate(R.id.action_chooserRecipientFragment_to_specifyAmountFragment)
+        }
+    }
 
 }
